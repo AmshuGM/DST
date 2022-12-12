124aa686ddb3e916027bd975be48f834aaaeff34
AmshuGM <99896640+AmshuGM@users.noreply.github.com>
2022-12-12 15:03:58 +0530

    Update linked list(amshu).c

--- linked list(amshu).c
+++ linked list(amshu).c
@@ -14,21 +14,76 @@ struct node * create(){
     return newnode=(struct node *)malloc(sizeof(struct node*));
 }
 
-struct node* beginsert(struct node* head){
+void beginsert(struct node** head){
     
-    struct node* temp=head;
+    struct node* temp=*head;
     struct node * newnode=create();
     printf("enter data: ");
     scanf("%d",&(newnode->data));
-    head=newnode;
+    *head=newnode;
     newnode->next=temp;
     printf("\n");
     
-    return head;
+   
+}
+
+void middleinsert(struct node** head,int i){
     
+    struct node* temp=*head;
+    int choice;
+    if(*head==NULL){
+    printf("linked list is empty\nDo you want to create a linked list?(1:yes,0:no):");
+    scanf("%d",&choice);
+    printf("\n");
+    }
+    else{
+    printf("enter the position :");scanf("%d",&i);
+    if(choice==0)printf("cannot add at 0\n");
+    else if(choice==1){
+       beginsert(head);
+        
+    }
     
+   else if(i==1){printf("inserting at the beginning...\n");beginsert(head);}
+    else{
+    i-=2;
+    while(i--){
+        temp=temp->next;
+        
+    }
+    struct node *hold=temp->next;
+    temp->next=create();
+     temp=temp->next;
+    printf("enter data: ");
+    scanf("%d",&(temp->data));
+   
+    temp->next=hold;
+    
+    }}
     
 }
+
+void endinsert(struct node ** head){
+    
+    struct node* temp=*head;
+    if(temp==NULL){printf("liked list is empty so element will be added at the beginning\n");
+        beginsert(head);
+    }
+    else{
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=create();
+    temp=temp->next;
+    temp->next=NULL;
+    printf("enter data: ");
+    scanf("%d",&(temp->data));
+    
+}
+}
+
+
+
 
 void display(struct node * temp){
     printf("Linked list : ");
@@ -44,15 +99,18 @@ while(temp!=NULL){
 int main() {
     struct node * head=NULL;
     int c;
+    int i;
     while(1){
-    printf("MAIN MENU\n-----------------\n1)INSERT AT BEGINNING\n2)DISPLAY\n3)EXIT\n\n");
+    printf("MAIN MENU\n-----------------\n1)INSERT AT BEGINNING\n2)INSERT AT THE MIDDLE\n3)INSERT AT THE END\n4)DISPLAY\n5)EXIT\n\n");
     scanf("%d",&c);
     switch(c){
-        case 1:head=beginsert(head);
+        case 1:beginsert(&head);
                 break;
-        case 2:display(head);printf("\n");
+        case 2:middleinsert(&head,i);break;
+        case 3:endinsert(&head);break;
+        case 4:display(head);printf("\n");
         break;
-        case 3:exit(0);
+        case 5:exit(0);
         
     }}
     return 0;
